9eaf28d39bec12a4430bc3e5e259bed8197eed54
Doug Torrance <dtorrance@piedmont.edu>
2026-01-02 21:00:10 -0500

    Use UMD (Universal Module Definition) pattern so we can import
    https://github.com/umdjs/umd

--- js/BootSideMenu.js
+++ js/BootSideMenu.js
@@ -4,7 +4,35 @@
  * http://www.lombardoandrea.com
  * https://github.com/AndreaLombardo/BootSideMenu
  * */
-(function ($) {
+
+// Universal Module Definition (https://github.com/umdjs/umd)
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
 
     // here we go!
     $.BootSideMenu = function(element, userOptions) {
@@ -514,4 +542,4 @@
          }
       });
     }
-}(jQuery));
+}));
